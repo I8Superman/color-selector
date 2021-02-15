58f19ee6767cb2e8dfc2812facec15a533f07959
Simon Andersen <simo414u@stud.kea.dk>
2021-02-15 19:16:02 +0100

    Basic CSS and HTML in place

--- color_selector.js
+++ color_selector.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log('Yerp!');
+console.log("Yerp!");
+const qs = (s) => document.querySelector(s);
+
+window.addEventListener('DOMContentLoaded', init);
+
+function init() {
+  const input = qs("input");
+  input.addEventListener('input', handleInput);
+}
+
+function handleInput(e) {
+
+  console.log(this.value);
+}
